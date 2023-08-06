e3230deeae3745fed3860ec29b77f860754b3851
Sanghyun Song <afew9man@gmail.com>
2023-08-07 06:31:47 +0900

    add some codes

--- rpsGame.py
+++ rpsGame.py
@@ -6,3 +6,14 @@ print('ROCK, PAPER, SCISSORS')
 wins = 0
 losses = 0
 ties = 0
+
+while True: # The main game loop.
+    print('%s Wins, %s Losses, %s Ties' % (wins, losses, ties))
+    while True: # The player input loop.
+        print('Enter your move: (r)ock (p)aper (s)cissors or (q)uit')
+        playerMove = input()
+        if playerMove == 'q':
+            sys.exit() # Quit the program.
+        if playerMove == 'r' or playerMove == 'p' or playerMove == 's':
+            break # Break out of the player input loop.
+        print('Type one of r, p, s, or q.')
